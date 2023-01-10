41bb586f78d4c7845c61cf09f58b9b8669c57777
Wong Ding hang <wding-ha@u82z05s02.42kl.edu.my>
2023-01-10 17:49:35 +0800

    done with operator overload for iterator

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME		= test
+
+SRC_DIR		= ./
+OBJ_DIR		= obj
+INC_DIR		= inc
+
+SRC_FILES	= test.cpp
+
+GCC 		= c++
+CFLAGS		= -Wall -Wextra -Werror
+C++FLAGS	= -std=c++98
+SANITIZE	= -fsanitize=address -g3
+RM			= rm -f
+
+OBJ_FILES	= $(addprefix , $(SRC_FILES:.c=.o))
+
+all:		$(NAME)
+
+%.o:		$(SRC_DIR)/%.c
+	@mkdir -p obj
+	$(GCC) $(C++FLAGS) $(CFLAGS) -I $(INC_DIR) -c $< -o $@
+
+$(LIBFT):
+	make -C $(LIBFT_DIR)
+
+$(MLX):
+	make -C $(MLX_DIR)
+
+${NAME}:	${LIBFT} $(MLX) ${OBJ_FILES} ${OBJ_BANNER}
+	${GCC} -o ${NAME} ${OBJ_FILES} $(SANITIZE)
+
+test: $(NAME)
+
+clean:
+	$(RM) $(OBJ_DIR)/*
+
+fclean:
+	$(RM) $(NAME)
+	$(RM) $(OBJ_DIR)/*
+	$(RM) output.log
+
+re:			fclean all
+
+.PHONY:		all clean fclean re
--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -1,35 +1,44 @@
 #ifndef FT_ITERATOR_HPP
 #define FT_ITERATOR_HPP
 
+#include <iostream>
+
 namespace	ft
 {
-	class iterator
-	{
-	private:
-		/* data */
+	template <typename Type>
+	class random_access_iterator{
 	public:
-		iterator();
-		~iterator();
-		iterator(const iterator &a);
-		iterator&	operator=(const iterator &a);
-		iterator&	operator==(const iterator &a);
-		iterator&	operator!=(const iterator &a);
-		iterator&	operator*(const iterator &a);
-		iterator&	operator++(void);
-		iterator&	operator++(int);
-		iterator&	operator--(void);
-		iterator&	operator--(int);
-		iterator&	operator+(int);
-		iterator&	operator+(const iterator &a);
-		iterator&	operator-(int);
-		iterator&	operator-(const iterator &a);
-		iterator&	operator>(const iterator &a);
-		iterator&	operator<(const iterator &a);
-		iterator&	operator>=(const iterator &a);
-		iterator&	operator<=(const iterator &a);
-		iterator&	operator+=(const iterator &a);
-		iterator&	operator-=(const iterator &a);
-		iterator&	operator[](int);
+        typedef Type       value_type;
+    	typedef Type*  		pointer;
+		typedef Type&		reference;
+		typedef ptrdiff_t	difference_type;
+
+		random_access_iterator();
+		~random_access_iterator();
+		random_access_iterator(pointer p);
+		random_access_iterator(const random_access_iterator &a);
+		random_access_iterator&	operator=(const random_access_iterator &a);
+
+		random_access_iterator::reference	operator*(void);
+		random_access_iterator&	operator++(void);
+		random_access_iterator&	operator++(int);
+		random_access_iterator&	operator--(void);
+		random_access_iterator&	operator--(int);
+		random_access_iterator	operator+(int);
+		random_access_iterator	operator-(int);
+		random_access_iterator::difference_type	operator-(random_access_iterator &a);
+		bool	operator!=(const random_access_iterator &a);
+		bool	operator==(const random_access_iterator &a);
+		bool	operator>(const random_access_iterator &a);
+		bool	operator<(const random_access_iterator &a);
+		bool	operator>=(const random_access_iterator &a);
+		bool	operator<=(const random_access_iterator &a);
+		random_access_iterator&	operator+=(int);
+		random_access_iterator&	operator-=(int);
+		random_access_iterator::reference	operator[](int);
+		
+		private:
+			pointer	_p;
 	};
 };
 
